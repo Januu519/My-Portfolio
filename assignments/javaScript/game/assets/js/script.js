d27a168804615fd4c4b7d1ec0348b86109d8180f
unknown <jayaminisakunthala629@gmail.com>
2023-06-08 21:43:45 +0530

    Content updated

--- assignments/javaScript/game/assets/js/script.js
+++ assignments/javaScript/game/assets/js/script.js
@@ -1,4 +1,4 @@
-var girl = document.getElementById("girl"), idleImageNumber, idleAnimationNumber;
+var girl = document.getElementById("girl"), idleImageNumber, idleAnimationNumber, runImageNumber, runAnimationNumber;
 idleImageNumber=1;
 idleAnimationNumber=0;
 
@@ -36,28 +36,214 @@ function runAnimationStart() {
 }
 
 
+
+jumpAnimationNumber=0;
+jumpImageNumber=1;
+girlMarginTop=307;
+function jumpAnimation() {
+
+    jumpImageNumber=jumpImageNumber+1;
+
+    if (jumpImageNumber<=6){
+        girlMarginTop=girlMarginTop-45;
+        girl.style.marginTop=girlMarginTop+"px";
+    }
+
+    if (jumpImageNumber>=7){
+        girlMarginTop=girlMarginTop+45;
+        girl.style.marginTop=girlMarginTop+"px";
+    }
+
+
+    if (jumpImageNumber==11){
+        jumpImageNumber=1;
+        clearInterval(jumpAnimationNumber);
+        jumpAnimationNumber=0;
+        runImageNumber=0;
+        runAnimationStart();
+    }
+    girl.src="assets/image/Jump ("+jumpImageNumber+").png"
+}
+function jumpAnimationStart() {
+    clearInterval(idleAnimationNumber);
+    runImageNumber=0;
+    clearInterval(runAnimationNumber);
+    jumpAnimationNumber=setInterval(jumpAnimation,200);
+
+}
+
+boxMarginLeft=1040;
+function createBoxes() {
+
+    for (var i=0; i<=10; i++) {
+
+        var box = document.createElement("div");
+        box.className = "box";
+        document.getElementById("background").appendChild(box);
+        box.style.marginLeft = boxMarginLeft + "px";
+        box.id="box"+i;
+
+        // boxLeft=boxLeft+500;
+        if (i<5){
+            boxMarginLeft=boxMarginLeft+2000;
+        }
+        if (i>=5){
+            boxMarginLeft=boxMarginLeft+1000;
+        }
+    }
+}
+
+
+var boxAnimationID=0;
+function boxAnimation() {
+
+    for (var i=0; i<10; i++){
+        var box=document.getElementById("box"+i);
+        var currentMarginLeft=getComputedStyle(box).marginLeft;
+        var newMarginLeft=parseInt(currentMarginLeft)-35;
+        box.style.marginLeft=newMarginLeft+"px";
+
+        if (newLeft>= -110 && newLeft<=100){
+            if (girlTop>300){
+                clearInterval(boxAnimationID);
+
+                clearInterval(runAnimationNumber);
+                runAnimationNumber=-1;
+
+                clearInterval(jumpAnimationNumber);
+                jumpAnimationNumber=-1;
+
+                clearInterval(moveBackgroundAnimationId);
+                moveBackgroundAnimationId=-1;
+
+                deadAnimationNumber=setInterval(girlDeadAnimation,150);
+            }
+        }
+
+    }
+}
+
+deadImageNumber=1;
+deadAnimationNumber=0;
+function girlDeadAnimation() {
+
+    deadImageNumber=deadImageNumber+1;
+
+    if (deadImageNumber==11){
+        deadImageNumber=10;
+
+
+        document.getElementById("end").style.visibility="visible";
+        document.getElementById("endscore").innerHTML=score;
+    }
+
+
+    girl.src="assets/image/Dead("+deadImageNumber+").png";
+
+}
+
 function keyCheck(event) {
-    //alert(event.which);
-    // enter=13
+    /*alert(event.which);
+    enter=13
+    space=32*/
 
-  var keyCode = event.which;
+    var keyCode=event.which;
 
-  if (keyCode==13){
-      if (runAnimationNumber==0){
-          runAnimationStart();
-      }
-      if (moveBackgroundAnimationId==0){
-          moveBackgroundAnimationId=setInterval(moveBackground,100)
-      }
-  }
+    if (keyCode==13){
+        if (runAnimationNumber==0){
+            runAnimationStart();
+        }
+
+
+        if (moveBackgroundAnimationId==0){
+            moveBackgroundAnimationId=setInterval(moveBackground,180)
+        }
+        if (boxAnimationID==0){
+            boxAnimationID=setInterval(boxAnimation,180);
+        }
+    }
+    if (keyCode==32){
+        if (jumpAnimationNumber==0)
+        {
+            jumpAnimationStart();
+        }
+        if (moveBackgroundAnimationId==0){
+            moveBackgroundAnimationId=setInterval(moveBackground,180);
+        }
+        if (boxAnimationID==0){
+            boxAnimationID=setInterval(boxAnimation,180);
+        }
+    }
 }
 
 var backgroundImagePositionX=0;
 var moveBackgroundAnimationId=0;
 
+var score=0;
+
+var level=1;
+
 function moveBackground() {
+
     backgroundImagePositionX=backgroundImagePositionX-20;
-    document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX + "px";
+
+    document.getElementById("backgroundID").style.backgroundPositionX=backgroundImagePositionX+"px";
+
+    score=score+1;
+    document.getElementById("score").innerHTML=score;
 
 
+
+    if (score === 50){
+
+
+        level = level + 1 ;
+
+
+        if (level === 2){
+            document.getElementById('level').innerHTML = level;
+            document.getElementById('win').style.visibility = 'visible';
+
+            clearInterval(boxAnimationID);
+            clearInterval(runAnimationNumber);
+            clearInterval(moveBackgroundAnimationId);
+
+        }
+    }
+    if (score === 100){
+        level = level + 1 ;
+        document.getElementById('level').innerHTML = level;
+        if (level === 3){
+            document.getElementById('win').style.visibility = 'visible';
+            clearInterval(boxAnimationID);
+            clearInterval(runAnimationNumber);
+            clearInterval(moveBackgroundAnimationId);
+
+        }
+    }
+
+    if (score === 150){
+        level = level + 1 ;
+        document.getElementById('level').innerHTML = level;
+        if (level === 4){
+            document.getElementById('win').style.visibility = 'visible';
+            clearInterval(boxAnimationID);
+            clearInterval(runAnimationNumber);
+            clearInterval(moveBackgroundAnimationId);
+
+        }
+    }
+}
+
+
+
+function reload() {
+    location.reload();
+
+}
+function reload1() {
+
+    document.getElementById('win').style.visibility = 'hidden';
+    boxAnimationID = setInterval(boxAnimation,100);
+    moveBackgroundAnimationId = setInterval(moveBackground,100);
 }
